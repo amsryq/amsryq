b8253bdc1768d2ede3410b58059ee33394b13e55
Amsyar Rasyiq <82711525+amsyarasyiq@users.noreply.github.com>
2025-09-27 23:11:45 +0800

    chore: Cleanup

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -15,30 +15,7 @@ export default defineConfig({
       preset: tierPresets.large,
     },
   ],
-
-  async onSponsorsReady(sponsors) {
-    await writeFile(
-      'sponsors.json',
-      JSON.stringify(
-        sponsors
-          .filter((i) => i.privacyLevel !== 'PRIVATE')
-          .map((i) => {
-            return {
-              name: i.sponsor.name,
-              login: i.sponsor.login,
-              avatar: i.sponsor.avatarUrl,
-              amount: i.monthlyDollars,
-              link: i.sponsor.linkUrl || i.sponsor.websiteUrl,
-              org: i.sponsor.type === 'Organization',
-            }
-          })
-          .sort((a, b) => b.amount - a.amount),
-        null,
-        2,
-      ),
-    )
-  },
-
+  
   outputDir: './sponsorkit',
   formats: ['svg', 'png'],
 
